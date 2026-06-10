ef6ed40e49f9c721123981f6e1a68b8454286d97
xiaoze <hanze06@163.com>
2026-06-10 10:15:04 +0800

    Add vibration table library

--- rust/target/debug/deps/flexible_vibration_table-a653e2411918e2da.d
+++ rust/target/debug/deps/flexible_vibration_table-a653e2411918e2da.d
@@ -1,5 +1,5 @@
-C:\Projects\FlexibleVibrationTable\target\debug\deps\flexible_vibration_table-a653e2411918e2da.d: src\lib.rs
+C:\Projects\FlexibleVibrationTable\rust\target\debug\deps\flexible_vibration_table-a653e2411918e2da.d: src\lib.rs
 
-C:\Projects\FlexibleVibrationTable\target\debug\deps\libflexible_vibration_table-a653e2411918e2da.rmeta: src\lib.rs
+C:\Projects\FlexibleVibrationTable\rust\target\debug\deps\libflexible_vibration_table-a653e2411918e2da.rmeta: src\lib.rs
 
 src\lib.rs:
